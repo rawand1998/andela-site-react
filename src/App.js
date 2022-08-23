6686258ba06c9c9748fec5a2e45476fc6d6900db
rawand1998 <rawandgaradh@gmail.com>
2022-08-23 18:58:11 +0300

    color varibles

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 
 import './App.css'
 import Home from './Pages/Home'
+import Footer from './Components/Common/Footer/Footer'
 function App() {
   return (
     <div className="App">
 
-     <Home />
+     {/* <Home /> */}
+     <Footer />
     </div>
   );
 }
---
+++ src/Components/Common/Footer/Footer.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import './Style.css'
+function Footer() {
+  return (
+    <div className="Footer-container">
+<h1>gvtver</h1>
+    </div>
+  )
+}
+
+export default Footer
