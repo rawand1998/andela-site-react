ab5d81f068512b5dbf841a80157505cb6105dc6f
rawand1998 <rawandgaradh@gmail.com>
2022-08-23 09:29:13 +0300

    file structure

--- src/App.js
+++ src/App.js
@@ -1,16 +1,9 @@
-import Navbar from "./Components/NavBar/Navbar";
 
-import Header from "./Components/Header/Header";
-import Home from "./Pages/Home"
 import './App.css'
 function App() {
   return (
     <div className="App">
-    
-    {/* <Navbar /> */}
-    {/* <Header />
-     */}
-     <Home />
+
      
     </div>
   );
