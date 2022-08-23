8315c5da889216bb7a7e103163e5640285a4f98a
rawand1998 <rawandgaradh@gmail.com>
2022-08-23 23:45:22 +0300

    home slider

---
+++ src/Components/Common/Slider/Slider.js
@@ -0,0 +1,89 @@
+import React from "react";
+import "./style.css";
+function Slider() {
+  return (
+    <div className="slider-container">
+      <h2>
+        We’re in the business of <em>changing lives</em>
+      </h2>
+      <div className="slider">
+        <div className="slider-list">
+          <div className="slider-track">
+            <div className="slider-1 sliders">
+              <img src="https://andela.com/wp-content/uploads/2022/03/Logitech-Logo-2015-present-copy-1024x576.png" />
+
+              <p>
+                Andela provides excellent service and support, speedy
+                recruitment, and top-notch talent. They identified the perfect
+                match skillset-wise.{" "}
+              </p>
+            </div>
+            <div className="slider-2 sliders">
+              <img src="https://andela.com/wp-content/uploads/2022/03/branch-logo-kale-1.png" />
+
+              <p>
+                We love working with Andela. Overall we’ve had a great
+                experience with the engineering talent who have joined Branch.
+              </p>
+            </div>
+            <div className="slider-3 sliders">
+              <img src="https://andela.com/wp-content/uploads/2022/03/attune-1024x343.png" />
+
+              <p>
+                We are getting a very large bang for our buck. The resources I
+                have on my team are self-starters and are able to take direction
+                and execute.
+              </p>
+            </div>
+            <div className="slider-4 sliders">
+              <img src="https://andela.com/wp-content/uploads/2022/03/gopuff.png" />
+
+              <p>
+                The team has been very transparent when sourcing our needs and
+                if we need to change direction they are able to quickly turn
+                around candidates.
+              </p>
+            </div>
+            <div className="slider-5 sliders">
+              <img src="https://andela.com/wp-content/uploads/2022/03/Fluxx-logo-google-apps-copy.png" />
+
+              <p>
+                We have been working with our Andela teammates since 2018. They
+                are so committed, work incredibly well with our team.
+              </p>
+            </div>
+            <div className="slider-6 sliders">
+              <img src="https://andela.com/wp-content/uploads/2022/03/Fluxx-logo-google-apps-copy.png" />
+
+              <p>
+                We have been working with our Andela teammates since 2018. They
+                are so committed, work incredibly well with our team.
+              </p>
+            </div>
+            <div className="slider-7 sliders">
+              <img src="https://andela.com/wp-content/uploads/2022/03/attune-1024x343.png" />
+
+              <p>
+                We have been working with our Andela teammates since 2018. They
+                are so committed, work incredibly well with our team.
+              </p>
+            </div>
+            <div className="slider-8 sliders">
+              <img src="https://andela.com/wp-content/uploads/2022/03/Nebula-color-stacked-kale.png" />
+
+              <p>
+                We have been working with our Andela teammates since 2018. They
+                are so committed, work incredibly well with our team.
+              </p>
+            </div>
+          </div>
+        </div>
+      </div>
+  
+      
+
+    </div>
+  );
+}
+
+export default Slider;
--- src/Pages/Home/Home.js
+++ src/Pages/Home/Home.js
@@ -1,25 +1,27 @@
-import React from 'react'
-import Header from '../../Components/Common/Header/Header'
-import Navbar from '../../Components/Common/NavBar/Navbar'
+import React from "react";
+import Header from "../../Components/Common/Header/Header";
+import Navbar from "../../Components/Common/NavBar/Navbar";
+import Slider from "../../Components/Common/Slider/Slider";
 
-import SubHeader from '../../Components/Common/SubHeader/SubHeader'
-import './style.css'
+import SubHeader from "../../Components/Common/SubHeader/SubHeader";
+import "./style.css";
 
 function Home() {
   return (
     <div>
-        <Header className="home" id="header-conatiner">
-            <Navbar />
-            <SubHeader title="Find work that you
-love with companies
-you can trust"  description="Join Andela to experience the joy of long-term work,
-with vetted companies and competitive compensation."/>
-
-        </Header>
-        <a className="btn">Find Work</a>
-        
+      <Header className="home" id="header-conatiner">
+        <Navbar />
+        <SubHeader
+          title="Find work that you love with companies you can trust"
+          description="Join Andela to experience the joy of long-term work,
+                        with vetted companies and competitive compensation."
+        />
+      </Header>
+      <a className="btn">Find Work</a>
+      <div className="spacer"></div>
+      <Slider />
     </div>
-  )
+  );
 }
 
-export default Home
+export default Home;
