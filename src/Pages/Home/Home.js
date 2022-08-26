58fa8338dbb7eaf32ea48a24d32699c62a63b0c1
rawand1998 <rawandgaradh@gmail.com>
2022-08-26 15:15:16 +0300

    convert looking for component into common component

--- src/Pages/Home/Home.js
+++ src/Pages/Home/Home.js
@@ -42,7 +42,56 @@ function Home() {
         </div>
       </div>
       <div className="spacer"></div>
-      <LookingFor />
+      <LookingFor
+        img=
+        {<img src="https://andela.com/wp-content/uploads/2022/03/shutterstock_1660490494-1024x683.jpg"/>}
+        
+        content={
+          <>
+            <h3>Grow your career with us</h3>
+            <ul>
+              <li>
+                Find meaningful, long-term work with interesting organizations
+              </li>
+              <li>Work with an internationally distributed team and company</li>
+              <li>
+                Gain additional skills through exclusive learning opportunities
+              </li>
+              <li>
+                Network with other technologists to develop your expertise
+              </li>
+            </ul>
+          </>
+        }
+        img2=
+       {<img src="https://andela.com/wp-content/uploads/2022/03/shutterstock_1696088776-768x512.jpg"/>}
+        
+        content2={
+          <>
+            <h3>Grow your career with us</h3>
+                <ul>
+                    <li>Only work with companies we trust</li>
+                    <li>Receive payments on time in your local currency, USD, or in Crypto</li>
+                    <li>Discover salaries that match your skills and experience</li>
+                   
+                </ul> 
+          </>
+        }
+        img3=
+        {<img src="https://andela.com/wp-content/uploads/2022/03/shutterstock_1653288229-1024x683.jpg" alt="not found"/>}
+        
+        content3={
+          <>
+          <h3>Grow your career with us</h3>
+                <ul>
+                    <li>Ditch the commute and work from anywhere</li>
+                    <li>With roles across different time zones, you choose when you work</li>
+                    <li>Gain additional skills through exclusive learning opportunities</li>
+                   
+                </ul>
+          </>
+        }
+      />
       <div className="spacer-1"></div>
       <Communction />
       <Skills />
@@ -70,10 +119,14 @@ function Home() {
       <CommitSuccess />
       <div className="spacer-1"></div>
       <Contected
-        title={<h2>Connecting your 
-          <em>brilliance</em><br/>
-          with opportunity</h2>}
-        
+        title={
+          <h2>
+            Connecting your
+            <em>brilliance</em>
+            <br />
+            with opportunity
+          </h2>
+        }
         buttonTitle="Find Work"
       />
       <Footer />
