c3c4e594ce5d704d858de74689519403f56071ff
rawand1998 <rawandgaradh@gmail.com>
2022-08-26 14:35:26 +0300

    client header

--- src/Pages/ClientPage/Clients.js
+++ src/Pages/ClientPage/Clients.js
@@ -1,10 +1,24 @@
 import React from 'react'
 import './style.css'
-
+import Header from '../../Components/Common/Header/Header'
+import Footer from "../../Components/Common/Footer/Footer";
+import SubHeader from "../../Components/Common/SubHeader/SubHeader";
+import Navbar from "../../Components/Common/NavBar/Navbar";
 function Clients() {
   return (
     <div>
-        
+            <Header className="home" id="header-conatiner">
+        <Navbar />
+        <SubHeader
+          title="
+          Scale your business
+          with technologists
+          you can trust"
+          description="Accelerate innovation with vetted technical talent."
+        />
+      </Header>
+      <a className="btn">Discover Talent</a>
+    
     </div>
   )
 }
