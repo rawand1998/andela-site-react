ae3dd9bfabea4e130ad9c92b062002abb70b1cb0
rawand1998 <rawandgaradh@gmail.com>
2022-08-27 15:22:35 +0300

    about columns section

--- src/Pages/About/About.js
+++ src/Pages/About/About.js
@@ -166,6 +166,43 @@ function About() {
         </div>
       </div>
       <div className="spacer"></div>
+      <div className="about-columns">
+        <div className="about-columns1">
+            <h2>We’ve achieved<br/>many <em>great things</em></h2>
+            <div style={{ height:'500px'}}></div>
+        </div>
+        <div className="about-columns2">
+            <ol className="start-2022">
+            <li>
+                <strong>FUTURE</strong> <br/><strong>We release our open marketplace</strong><br/><br/>We unlock the ability for our users to interact directly with one another while launching new value-added services like healthcare.
+            </li>
+            <li>
+                <strong>FUTURE</strong> <br/><strong>We release our open marketplace</strong><br/><br/>We unlock the ability for our users to interact directly with one another while launching new value-added services like healthcare.
+            </li>
+            <li>
+                <strong>FUTURE</strong> <br/><strong>We release our open marketplace</strong><br/><br/>We unlock the ability for our users to interact directly with one another while launching new value-added services like healthcare.
+            </li>
+            <li>
+                <strong>FUTURE</strong> <br/><strong>We release our open marketplace</strong><br/><br/>We unlock the ability for our users to interact directly with one another while launching new value-added services like healthcare.
+            </li>
+            <li>
+                <strong>FUTURE</strong> <br/><strong>We release our open marketplace</strong><br/><br/>We unlock the ability for our users to interact directly with one another while launching new value-added services like healthcare.
+            </li>
+            <li>
+                <strong>FUTURE</strong> <br/><strong>We release our open marketplace</strong><br/><br/>We unlock the ability for our users to interact directly with one another while launching new value-added services like healthcare.
+            </li>
+            <li>
+                <strong>FUTURE</strong> <br/><strong>We release our open marketplace</strong><br/><br/>We unlock the ability for our users to interact directly with one another while launching new value-added services like healthcare.
+            </li>
+            <li>
+                <strong>FUTURE</strong> <br/><strong>We release our open marketplace</strong><br/><br/>We unlock the ability for our users to interact directly with one another while launching new value-added services like healthcare.
+            </li>
+            <li>
+                <strong>FUTURE</strong> <br/><strong>We release our open marketplace</strong><br/><br/>We unlock the ability for our users to interact directly with one another while launching new value-added services like healthcare.
+            </li>
+            </ol>
+        </div>
+      </div>
     </div>
   );
 }
