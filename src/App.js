c67bc6b2320fa7e599c0f4c39366839208be4124
rawand1998 <rawandgaradh@gmail.com>
2022-08-29 11:47:48 +0300

    elevate skill compoennt responsive

--- src/App.js
+++ src/App.js
@@ -16,7 +16,7 @@ function App() {
   return (
     <div className="App">
 
-     <Home />
+     {/* <Home /> */}
      {/* <Footer /> */}
      {/* <Enterprise /> */}
      {/* <About /> */}
@@ -25,7 +25,7 @@ function App() {
      {/* <Register/> */}
 
      {/* <FindWork /> */}
-     {/* <Clients /> */}
+     <Clients />
      {/* <Contected /> */}
 
     </div>
