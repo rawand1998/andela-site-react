b56c2072b8fa9091cd543a9f4c86118bef91d809
rawand1998 <rawandgaradh@gmail.com>
2022-08-31 09:50:34 +0300

    fix responsive navbar

--- src/Components/Common/ScaleTeam/ScaleTeam.jsx
+++ src/Components/Common/ScaleTeam/ScaleTeam.jsx
@@ -1,14 +1,12 @@
 import React from "react";
 import "./style.css";
-function ScaleTeam({ img, content ,bg }) {
+function ScaleTeam({ img, content, bg }) {
   return (
-  
-      <div className={`scale-team-container ${bg}`}>
-        <figure>{img}</figure>
+    <div className={`scale-team-container ${bg}`}>
+      <figure>{img}</figure>
 
-        <div class="scale-wp-block-column">{content}</div>
-      </div>
-  
+      <div class="scale-wp-block-column">{content}</div>
+    </div>
   );
 }
 
