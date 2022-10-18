0fdf085b2a67410bb622fdc9efee3210b6ac3abc
rawand1998 <rawandgaradh@gmail.com>
2022-10-18 10:56:18 +0300

    delete navbar

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,10 @@ import {  Routes, Route } from "react-router-dom";
 import React , {Suspense} from 'react'
 import "./App.css";
 import Router from "./routes/Router";
-import Navbar from "./Components/Common/NavBar/Navbar";
 function App() {
   return (
   
       <Suspense fallback={<p>loading...</p>}>
-      <Navbar/>
       <Routes>
         {Router.routes.map((route, Index) => (
           <Route path={route.path} element={route.element} key={Index} />
