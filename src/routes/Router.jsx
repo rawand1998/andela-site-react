c418eb7aee954b7c9504f5f7de01cf511f91ec5d
rawand1998 <rawandgaradh@gmail.com>
2022-10-19 16:12:45 +0300

    fix height

--- src/routes/Router.jsx
+++ src/routes/Router.jsx
@@ -1,23 +1,20 @@
-
-
 import React, { lazy } from "react";
+import Clients from "../Pages/Client/Clients";
 const Register = lazy(() => import("../Pages/Auth/register/Register"));
 const Login = lazy(() => import("../Pages/Auth/Login/Login"));
 const About = lazy(() => import("../Pages/About/About"));
-const Clients = lazy(() => import("../Pages//ClientPage/Clients"));
-const Enterprise = lazy(() =>
-  import("../Pages/Enterprise/Enterprise")
-);
-const FindWork = lazy(() => import("../Pages/FindWorkPage/FindWork"));
+const Enterprise = lazy(() => import("../Pages/Enterprise/Enterprise"));
+const FindWork = lazy(() => import("../Pages/FindWork/FindWork"));
 const Error = lazy(() => import("../Pages/Error"));
-const Home =lazy(()=>import('../Pages/Home/Home'))
+const Home = lazy(() => import("../Pages/Home/Home"));
+
 const Router = {
   routes: [
     { path: "/", element: <Home /> },
     { path: "about", element: <About /> },
     { path: "clients", element: <Clients /> },
     { path: "enterprise", element: <Enterprise /> },
-    { path: "login", element: <Login />},
+    { path: "login", element: <Login /> },
     { path: "register", element: <Register /> },
     { path: "findWork", element: <FindWork /> },
     { path: "*", element: <Error /> },
